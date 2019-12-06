28c20e6849675cd84529414c33036885be3f81a0
Kevin Krato <kratok08@gmail.com>
2019-12-06 10:35:36 -0700

    Create o-t-m relations in index feature tests

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "/students" do
   before :each do
+    @course_1 = Course.create(name: "Potions")
+    @course_2 = Course.create(name: "Dark Arts")
+    @course_3 = Course.create(name: "Muggle Studies")
+
     @student_1 = Student.create(
                               name: "Harry Pooter", 
                               age: 17, 
@@ -12,6 +16,12 @@ RSpec.describe "/students" do
                               age: 16, 
                               house: "GriffyDoor"
                             )
+
+    @course_1.students << @student_1
+    @course_2.students << @student_1
+    @course_3.students << @student_1
+    @course_1.students << @student_2
+    @course_2.students << @student_2
   end
 
   it "I see a list of students with the following information: name, age, house" do
@@ -28,5 +38,6 @@ RSpec.describe "/students" do
       expect(page).to have_content(@student_2.age)
       expect(page).to have_content(@student_2.house)
     end
+
   end
 end
